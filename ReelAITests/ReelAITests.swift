3f93683e21d6353ef049b3615aaba7e3f8ce6c59
Patrice Azi <pat@movingzoom.com>
2025-02-03 13:59:14 -0600

    Initial Commit

---
+++ ReelAITests/ReelAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelAITests.swift
+//  ReelAITests
+//
+//  Created by Patrice Azi on 2/3/25.
+//
+
+import Testing
+@testable import ReelAI
+
+struct ReelAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
